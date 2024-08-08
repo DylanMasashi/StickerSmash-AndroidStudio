88be8609834bdf0f130a42d031a3f1b2d73b9dd7
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-08-07 22:49:00 -0300

    07/08

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from './components/Button';
-import ImageViewer from './components/ImageViewer';
+import { StyleSheet, View } from 'react-native';
 
-const PlaceholderImage = require ('./assets/images/background-image.png');
+ import ImageViewer from './components/ImageViewer'; 
+ import Button from './components/Button';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource= {PlaceholderImage}/>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+      <Button theme="primary" label="Choose a photo" />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
@@ -26,21 +29,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
   },
-
   imageContainer: {
     flex: 1,
     paddingTop: 58,
   },
-
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
-
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
   },
 });
-
